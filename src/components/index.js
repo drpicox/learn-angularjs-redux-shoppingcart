1b124e1d6eab24dc9353c3832dd8e3bcf8fdc2e3
David Rodenas <david.rodenas+github@gmail.com>
2017-06-26 01:06:11 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 angular.module('shoppingCartApp.components', [
 		'shoppingCartApp.components.App',
-		'shoppingCartApp.components.CartView',
-		'shoppingCartApp.components.CatalogView',
 		'shoppingCartApp.components.Header',
 		'shoppingCartApp.components.Link',
 		'shoppingCartApp.components.Product',
